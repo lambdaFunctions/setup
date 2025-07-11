bb2f189111bb10e3f47814ed463a8343a546fb4a
Paulo Vinicius <paulo@macos.local>
2025-07-10 20:28:19 -0400

    setting up rust analyzer

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,7 +118,29 @@ keymap.set("n", "<leader>lw", "<cmd>LspRename<CR>", { desc = "Rename in entire o
 
 -- LSPs
 vim.lsp.enable('lua_ls')                -- brew install lua-language-server
-vim.lsp.enable('rls')
--- vim.lsp.enable('rust-analyzer')      -- brew install rust-analyzer
+vim.lsp.enable('rust-analyzer')      -- brew install rust-analyzer
 vim.lsp.enable('pyright')               -- brew install pyright
+
+vim.lsp.config.rust_analyzer = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { 'rust-analyzer' },
+    filetypes = { 'rust' },
+    root_markers = {"Cargo.toml", ".git"},
+    single_file_support = true,
+    settings = {
+        ['rust-analyzer'] = {
+            diagnostics = {
+                enable = false;
+            }
+        }
+    },
+    before_init = function(init_params, config)
+        -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
+        if config.settings and config.settings['rust-analyzer'] then
+            init_params.initializationOptions = config.settings['rust-analyzer']
+        end
+    end,
+}
+vim.lsp.enable("rust_analyzer")
 
